76de2ee4365f31e62cef95be7f13623bd4ba9a5c
Asifa <161161856+Asifa0121@users.noreply.github.com>
2024-03-13 23:21:37 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//Lower Case
+{
+    var personname = "Governer";
+    console.log(personname.toLowerCase());
+}
+//Upper Case
+{
+    var personname = "Governer";
+    console.log(personname.toUpperCase());
+}
+//Title Case
+{
+    var personname = "Governer";
+    console.log(personname.charAt(0).toUpperCase() + personname.slice(1).toLowerCase());
+}
---
+++ main.ts
@@ -0,0 +1,17 @@
+//Lower Case
+
+{
+    let personname : string = "Governer";
+console.log(personname.toLowerCase());
+}
+//Upper Case
+{
+    let personname : string = "Governer";
+console.log(personname.toUpperCase());
+}
+
+//Title Case
+{
+    let personname : string = "Governer";
+console.log(personname.charAt(0).toUpperCase() + personname.slice(1).toLowerCase());
+}
